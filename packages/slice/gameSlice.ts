cc48e4da90b27ba3ca4aa316a0ea31af42643f12
bryanh210 <tuongh@sas.upenn.edu>
2025-03-11 23:00:03 +0700

    progress: adding initial state for gameslice

--- packages/slice/gameSlice.ts
+++ packages/slice/gameSlice.ts
@@ -1,8 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type Position = {
+  x: number;
+  y: number
+}
+
+type Sound = {
+  id: string,
+  playedAt: Position
+}
+
+type userAction = {
+  timeStamp: number,
+  sound: boolean,
+  position: boolean
+}
+
+type GameState = {
+  level: number,
+  positionHistory: Position[],
+  soundHistory: Sound[],
+  score: number,
+  userAction: userAction[],
+  timeBeforeAnswer: number,
+  successThreshold: number,
+  latestAllowedResponseTime: number
+}
+
+const initialState: GameState = {
+  level: 1,
+  positionHistory: [],
+  soundHistory: [],
+  score: 0,
+  userAction: [],
+  timeBeforeAnswer: 1500,
+  successThreshold: 80,
+  latestAllowedResponseTime: 1498
+}
+
 const gameSlice = createSlice({
   name: "game",
-  initialState: { level: 1, score: 0 },
+  initialState,
   reducers: {
     resetGame: (state) => { state.level = 1; state.score = 0; },
   },
